19866ced478a9304df7dc3fa4671ebdfd19262e7
Tao Qingyun <845767657@qq.com>
2017-12-09 20:02:39 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='multpreduce',
+    version='0.1.0',
+    description='Multiprocess Reduce',
+    author='Tao Qingyun',
+    author_email='845767657@qq.com',
+    url='https://github.com/qingyunha/multpreduce',
+    py_modules = ['multpreduce'],
+    license='MIT',
+    )
